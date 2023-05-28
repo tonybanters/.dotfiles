217bbd072419f510d199e13ece82a46f0fd9a16e
tonybanters <tonyoutoften@gmail.com>
2023-05-27 22:35:36 -0700

    Fixed spacing.

--- nvim/lua/tony/packer.lua
+++ nvim/lua/tony/packer.lua
@@ -29,23 +29,22 @@ return require('packer').startup(function(use)
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
   use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = { 
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},      -- Required
-		  {                                      -- Optional
+	'VonHeikemen/lsp-zero.nvim',
+	branch = 'v2.x',
+	requires = { -- LSP Support
+		{'neovim/nvim-lspconfig'},      -- Required
+		{                                      -- Optional
 		  'williamboman/mason.nvim',
-		  run = function()
-			  pcall(vim.cmd, 'MasonUpdate')
-		  end,
-	  },
-	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+		    run = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+        },
+	    {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-	  -- Autocompletion
-	  {'hrsh7th/nvim-cmp'},     -- Required
-	  {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	  {'L3MON4D3/LuaSnip'},     -- Required
-      }
+	    -- Autocompletion
+	    {'hrsh7th/nvim-cmp'},     -- Required
+	    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+	    {'L3MON4D3/LuaSnip'},     -- Required
+    }
   }
 end)
